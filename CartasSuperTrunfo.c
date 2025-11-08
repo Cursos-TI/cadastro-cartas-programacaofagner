c18cf15fbf188deb52e3a0fd6905f3be81330e73
Fagner Antunes <fagninhocolorado@gmail.com>
2025-11-08 00:49:46 +0000

    Cadastro das cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,7 +6,7 @@ int main() {
   //Carta 1 - Declaração de variáveis
 
     char estado_1; // Letra que representa o estado CARTA 1
-    char codigo_carta1[20]; // Código da carta (Letra que representa o estado seguido de um número de 01 à 04) CARTA 1
+    char codigo_carta1[4]; // Código da carta (Letra que representa o estado seguido de um número de 01 à 04) CARTA 1
     char cidade_1[20]; // Nome da cidade CARTA 1
     int populacao_1, pontos_tur_1; // População CARTA 1 | N° pontos turísticos CARTA 1
     float area_1, pib_1; // Área da cidade CARTA 1 | PIB cidade CARTA 1
@@ -27,7 +27,66 @@ int main() {
     printf("Digite o nome da cidade: \n"); // Envia informação para a tela do usuário
     scanf("%s", &cidade_1); // Coleta dado digitado pelo usuário
 
-  // Área para exibição dos dados da cidade
+    // POPULAÇÃO 
+    printf("Digite a população: \n"); // Envia informação para a tela do usuário
+    scanf("%d", &populacao_1); // Coleta dado digitado pelo usuário
+
+    // ÁREA
+    printf("Digite a área em km²: \n"); // Envia informação para a tela do usuário
+    scanf("%f", &area_1); // Coleta dado digitado pelo usuário
+
+    // PIB
+    printf("Digite o PIB: \n"); // Envia informação para a tela do usuário
+    scanf("%f", &pib_1); // Coleta dado digitado pelo usuário
+
+    // N° DE PONTOS TURÍSTICOS
+    printf("Digite o n° de pontos turísticos: \n"); // Envia informação para a tela do usuário
+    scanf("%f", &pontos_tur_1); // Coleta dado digitado pelo usuário
+    printf("\n"); // Pula linha entre cartas
+    
+    // --------------------------------------------------------------------------------------------------------------------------
+
+    //Carta 2 - Declaração de variáveis
+
+    char estado_2; // Letra que representa o estado CARTA 2
+    char codigo_carta2[4]; // Código da carta (Letra que representa o estado seguido de um número de 01 à 04) CARTA 2
+    char cidade_2[20]; // Nome da cidade CARTA 2
+    int populacao_2, pontos_tur_2; // População CARTA 2 | N° pontos turísticos CARTA 2
+    float area_2, pib_2; // Área da cidade CARTA 2 | PIB cidade CARTA 2
+
+    // Área para entrada de dados | Carta 2 - Coleta dados de entrada
+
+    printf("Insira os dados da CARTA 02! \n"); // Envia informação para a tela do usuário
+    
+    // LETRA ESTADO 02
+    printf("Digite a letra que representa o Estado: \n"); // Envia informação para a tela do usuário
+    scanf("%s", &estado_2); // Coleta dado digitado pelo usuário
+
+    // CÓDIGO CARTA 
+    printf("Digite o código da carta: \n"); // Envia informação para a tela do usuário
+    scanf("%s", &codigo_carta2); // Coleta dado digitado pelo usuário
+
+    // NOME DA CIDADE 
+    printf("Digite o nome da cidade: \n"); // Envia informação para a tela do usuário
+    scanf("%s", &cidade_2); // Coleta dado digitado pelo usuário
+
+    // POPULAÇÃO 
+    printf("Digite a população: \n"); // Envia informação para a tela do usuário
+    scanf("%d", &populacao_2); // Coleta dado digitado pelo usuário
+
+    // ÁREA
+    printf("Digite a área em km²: \n"); // Envia informação para a tela do usuário
+    scanf("%f", &area_2); // Coleta dado digitado pelo usuário
+
+    // PIB
+    printf("Digite o PIB: \n"); // Envia informação para a tela do usuário
+    scanf("%f", &pib_2); // Coleta dado digitado pelo usuário
+
+    // N° DE PONTOS TURÍSTICOS
+    printf("Digite o n° de pontos turísticos: \n"); // Envia informação para a tela do usuário
+    scanf("%f", &pontos_tur_2); // Coleta dado digitado pelo usuário
+
+    // Área para exibição dos dados da cidade
 
 return 0;
 } 
